115db3e0d136963a85a8c81984e767f1dcc4dcfa
Peter Guerrero <peterguerrero@Peters-MacBook-Pro.local>
2016-06-11 13:56:37 -0500

    first push

---
+++ calorie.js
@@ -0,0 +1,14 @@
+
+//  $(document).ready(function(){
+//     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+//      $('#open-model').on("click", function(){
+//      	$('#modal1').openModal();
+//      });
+     
+//      $(function() {
+//         $( "#datepicker" ).datepicker();
+//       });
+
+// $('select').material_select();
+
+//   });
